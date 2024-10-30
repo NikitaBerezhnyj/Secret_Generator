11340e3328812aa3f70d70cb4dfd0106117121f8
Nikita Berezhnyj <97048105+NikitaBerezhnyj@users.noreply.github.com>
2024-10-30 13:10:29 +0200

    (fix): Minnor bag with Roboto font links

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,37 +1,3 @@
-// import type { Metadata } from "next";
-// import localFont from "next/font/local";
-// import "./globals.css";
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
-
-// export const metadata: Metadata = {
-//   title: "Secret Generator",
-//   description: "Generator secure password and jwt secret",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="en">
-//       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
-
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -48,9 +14,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Підключення шрифта Roboto */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
           rel="stylesheet"
